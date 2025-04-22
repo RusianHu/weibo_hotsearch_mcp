e226b49248116305d40a17ca6aba826fca6e4c3d
RusianHu <hu_bo_cheng@qq.com>
2025-04-23 00:58:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="weibo-hotsearch-mcp",
-    version="0.1.0",
+    version="1.0.0",
     author="RusianHu",
     author_email="rusianhu@example.com",
     description="微博热搜MCP服务",
